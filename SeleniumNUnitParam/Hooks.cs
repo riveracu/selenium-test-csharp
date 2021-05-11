aef487aa37a39e49c61a93603e050dee32734b9d
Rafael Rivera Tablada <dere007@gmail.com>
2021-05-11 18:51:40 +0200

    Update Hooks.cs

--- SeleniumNUnitParam/Hooks.cs
+++ SeleniumNUnitParam/Hooks.cs
@@ -23,7 +23,7 @@ namespace SeleniumNUnitParam
         {
             //nunit3-console.exe --params:Browser=Firefox \SeleniumNUnitParam.dll
             //If nothing specified, test will run in Chrome browser
-            var browserType = TestContext.Parameters.Get("Browser", "Chrome");
+            var browserType = TestContext.Parameters.Get("Browser", "Firefox");
 
 
             _browserType = (BrowerType)Enum.Parse(typeof(BrowerType), browserType);
